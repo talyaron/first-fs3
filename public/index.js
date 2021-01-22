bc7d9260732569808405527a66d85aff33e119c1
Tal <tal.yaron@gmail.com>
2021-01-22 11:27:48 +0200

    cont

--- public/index.js
+++ public/index.js
@@ -1,1 +1,24 @@
 document.querySelector('h1').style.background = 'yellow';
+const root = document.querySelector('#root')
+
+
+function getUsers(e){
+    console.log('get users')
+
+    fetch('/get-users')
+    .then(r=>r.json())
+    .then(data=>{
+        console.log('then')
+        renderUsers(data)
+    })
+    console.log('after fetch')
+}
+
+function renderUsers(users){
+    let html = '';
+    users.forEach(user=>{
+        html += `<p>Name: ${user.name}</p>`
+    }) 
+
+    root.innerHTML = html
+}
--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+const users = [
+  {name:'Kilani', id:'222222222'},
+  {name:'Yara', id:'333333333'},
+  {name:'Ihab', id:'444444'}
+]
+
+//route
+app.get('/get-users',(req, res)=>{
+  res.send(users)
+})
+
+
 
 
 const port = process.env.PORT || 3000;
