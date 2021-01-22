e95839eb76d95a27f6d639658c86cca0400e3f13
Tal <tal.yaron@gmail.com>
2021-01-22 13:42:20 +0200

    more info

--- public/index.js
+++ public/index.js
@@ -1,24 +1,37 @@
 document.querySelector('h1').style.background = 'yellow';
-const root = document.querySelector('#root')
 
 
-function getUsers(e){
+
+function getUsers() {
     console.log('get users')
 
     fetch('/get-users')
-    .then(r=>r.json())
-    .then(data=>{
-        console.log('then')
-        renderUsers(data)
-    })
+        .then(r => r.json())
+        .then(users => {
+            console.log('then')
+            renderUsers(users)
+        })
     console.log('after fetch')
 }
 
-function renderUsers(users){
+function getPassword(name) {
+    console.log('get password', name)
+
+    fetch(`/get-password?name=${name}&city=arara`)
+        .then(r => r.json())
+        .then(pass => {
+
+            console.log(pass.password)
+        })
+    console.log('after fetch')
+}
+
+function renderUsers(users) {
+    const root = document.querySelector('#root');
     let html = '';
-    users.forEach(user=>{
-        html += `<p>Name: ${user.name}</p>`
-    }) 
+    users.forEach(user => {
+        html += `<p onclick="getPassword('${user.name}')">Name: ${user.name}</p>`
+    })
 
     root.innerHTML = html
 }
--- server.js
+++ server.js
@@ -5,20 +5,31 @@ const app = express();
 app.use(express.static('public'));
 
 const users = [
-  {name:'Kilani', id:'222222222'},
-  {name:'Yara', id:'333333333'},
-  {name:'Ihab', id:'444444'}
+  {name:'Kilani', password:'222222222'},
+  {name:'Yara', password:'333333333'},
+  {name:'Ihab', password:'444444'}
 ]
 
-//route
+//routes
 app.get('/get-users',(req, res)=>{
+ 
   res.send(users)
+})
+
+app.get('/get-password',(req, res)=>{
+  const {name} =req.query;
+
+  console.log(req.query);
+
+  let usr = users.find(user=>user.name === name);
+  console.log(usr)
+  res.send({password:usr.password})
 })
 
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.listen(port, function () {
   console.log('listening', port)
