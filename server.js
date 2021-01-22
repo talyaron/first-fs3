5e815eb26815ae45065c492ce5b0f050d3877602
Tal <tal.yaron@gmail.com>
2021-01-22 08:18:58 +0200

    init

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require('express');
 const app=express();
 const fs = require('fs');
 
+app.use(express.static('public'))
+
 app.get('/', (req, res)=>{
   try{
     fs.readFile('./index.html','utf8',(err,data)=>{
